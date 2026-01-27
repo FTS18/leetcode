10cf3f6901294bfa36a648b23ce8d95e0b2719ee
Ananay <73402363+FTS18@users.noreply.github.com>
2026-01-28 00:30:08 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.cpp
@@ -0,0 +1,10 @@
+// Last updated: 28/01/2026, 00:30:07
+class Solution {
+public:
+    int fib(int n) {
+        if (n==0){
+            return 0;
+        }else if (n==1)return 1;
+        return fib(n-1)+fib(n-2);
+    }
+};
