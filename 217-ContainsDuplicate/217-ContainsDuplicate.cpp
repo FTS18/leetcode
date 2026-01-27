393a50330fc1126ad5503bdeef6399bccf4ff888
Ananay <73402363+FTS18@users.noreply.github.com>
2026-01-28 00:30:39 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -0,0 +1,12 @@
+// Last updated: 28/01/2026, 00:30:39
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for (int i=0;i<nums.size()-1;i++){
+            if (nums[i]==nums[i+1]){
+                return true;
+            }
+        }return false;
+    }
+};
