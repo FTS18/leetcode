2e36198afc9a320a33cddc0f9f12da775b4fd6d3
Ananay <73402363+FTS18@users.noreply.github.com>
2026-01-29 01:00:07 +0530

    Create 3046-SplittheArray

---
+++ 3046-SplittheArray/3046-SplittheArray.cpp
@@ -0,0 +1,16 @@
+// Last updated: 29/01/2026, 01:00:07
+1class Solution {
+2public:
+3    bool isPossibleToSplit(vector<int>& nums) {
+4        map<int,int>m;
+5        for(auto i:nums){
+6            m[i]++;
+7        }
+8        for (auto i:m){
+9            cout<<i.first<<i.second<<endl;
+10            if (i.second>2){
+11                return false;
+12            }
+13        }return true;
+14    }
+15};
