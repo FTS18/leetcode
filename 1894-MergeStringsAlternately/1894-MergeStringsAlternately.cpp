1692d150bbb67724917cc0b2554402b789968bd7
Ananay <73402363+FTS18@users.noreply.github.com>
2026-02-04 01:36:37 +0530

    Create 1894-MergeStringsAlternately

---
+++ 1894-MergeStringsAlternately/1894-MergeStringsAlternately.cpp
@@ -0,0 +1,16 @@
+// Last updated: 04/02/2026, 01:36:37
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string s;
+        for (int i = 0; i < max(word1.size(), word2.size()); i++) {
+            if (i < word1.size()) {
+                s.push_back(word1[i]);
+            }
+            if (i < word2.size()){
+                s.push_back(word2[i]);
+        }
+    }
+    return s;}
+}
+;
