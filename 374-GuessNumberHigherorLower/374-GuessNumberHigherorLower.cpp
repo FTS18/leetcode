982492cee0b442acb53ef92aaa48548139142845
Ananay <73402363+FTS18@users.noreply.github.com>
2026-02-03 13:53:34 +0530

    File updated at 03/02/2026, 13:53:32

--- 374-GuessNumberHigherorLower/374-GuessNumberHigherorLower.cpp
+++ 374-GuessNumberHigherorLower/374-GuessNumberHigherorLower.cpp
@@ -1,4 +1,4 @@
-// Last updated: 03/02/2026, 13:52:48
+// Last updated: 03/02/2026, 13:53:32
 1/** 
 2 * Forward declaration of guess API.
 3 * @param  num   your guess
@@ -10,23 +10,21 @@
 9class Solution {
 10public:
 11    int guessNumber(int n) {
-12        int l = 1;
-13        int r = n;
-14        int m;
-15        while (l <= r) {
-16            m = l + (r - l) / 2;
-17            int a = guess(m);
-18            if (a == 0) {
-19                 return m;
-20            } 
-21            else if (a == -1) {
-22                r = m - 1;
-23            } 
-24            else {
-25            l = m + 1;
-26            }
-27        }
-28        return -1;
-29    }
-30};
-31
+12        int l = 1,r=n,m=0;
+13        while (l <= r) {
+14            m = l + (r - l) / 2;
+15            int a = guess(m);
+16            if (a == 0) {
+17                 return m;
+18            } 
+19            else if (a == -1) {
+20                r = m - 1;
+21            } 
+22            else {
+23            l = m + 1;
+24            }
+25        }
+26        return -1;
+27    }
+28};
+29
